2a391f78b1f6016921e88289c319c2bb2725a297
belik9@inbox.ru <belik9@inbox.ru>
2021-09-26 23:10:29 +0300

    Add columns with cards UI, add create card

--- App.js
+++ App.js
@@ -1,21 +1,159 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SectionList, TextInput, StyleSheet, Text, View, StatusBar, Modal, Pressable } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const App = () => {
+    const initialColumns = [
+        {
+            title: 'Колонка 1',
+            data: [
+                {
+                    title: 'Название',
+                    data: 'Text'
+                },
+                {
+                    title: 'Название',
+                    data: 'Text'
+                }
+            ],
+        },
+        {
+            title: 'Колонка 2',
+            data: [
+                {
+                    title: 'Название',
+                    data: 'Text'
+                },
+                {
+                    title: 'Название',
+                    data: 'Text'
+                }
+            ],
+        }
+    ];
+    const [columns, setColumns] = useState(initialColumns);
+    const [showColumns, setShowColumns] = useState(false);
+
+    const [showModal, setShowModal] = useState(false);
+    const initialData = {column: '', title: '', data: ''};
+    const [modalData, setModalData] = useState(initialData);
+
+    const addCard = () => {
+        const {column, ...data} = modalData;
+
+        setColumns(columns.map(item => item.title === modalData.column ? {
+            ...item,
+            data: [...item.data, data]
+        } : item));
+
+        setShowModal(false);
+        setModalData(initialData);
+    }
+
+    return (
+        <View style={styles.container}>
+            {showColumns ? (
+                <SectionList
+                    sections={columns}
+                    keyExtractor={(item, index) => index}
+                    renderItem={({section, item, index}) => (
+                        <View key={index}>
+                            <Text>{item.title}</Text>
+                            <Text style={styles.content}>{item.data}</Text>
+                            {index === section.data.length - 1 && (
+                                <Pressable onPress={() => setShowModal(true)}>
+                                    <Text style={styles.button}>+</Text>
+                                </Pressable>
+                            )}
+                        </View>
+                    )}
+                    renderSectionHeader={({section: {title}}) => (
+                        <Text style={styles.header}>{title}</Text>
+                    )}
+                />
+            ) : (
+                <Pressable onPress={() => setShowColumns(true)}>
+                    <View style={styles.menu}>
+                        <View style={styles.menuItem}/>
+                        <View style={styles.menuItem}/>
+                        <View style={styles.menuItem}/>
+                    </View>
+                </Pressable>
+            )}
+            <Modal
+                visible={showModal}
+                onRequestClose={() => setShowModal(false)}
+            >
+                <View style={styles.container}>
+                    <Text>Колонка</Text>
+                    <TextInput
+                        onChangeText={(value) => setModalData({...modalData, column: value})}
+                        value={modalData.column}
+                        placeholder="Название колонки"
+                    />
+                    <Text>Карточка</Text>
+                    <TextInput
+                        onChangeText={(value) => setModalData({...modalData, title: value})}
+                        value={modalData.title}
+                        placeholder="Название карточки"
+                    />
+                    <Text>Текст</Text>
+                    <TextInput
+                        onChangeText={(value) => setModalData({...modalData, data: value})}
+                        value={modalData.data}
+                        placeholder="Текст"
+                    />
+                    <Pressable onPress={addCard}>
+                        <Text style={styles.button}>Ok</Text>
+                    </Pressable>
+                </View>
+            </Modal>
+        </View>
+    );
+}
+
+const solidBorder = {
+    borderWidth: 1,
+    borderColor: '#000',
+    borderStyle: 'solid'
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        paddingTop: StatusBar.currentHeight + 15,
+        display: "flex",
+        flexDirection: 'column',
+        paddingHorizontal: 15,
+    },
+    header: {
+        fontSize: 25,
+        marginTop: 40,
+        padding: 5,
+        ...solidBorder
+    },
+    content: {
+        padding: 40,
+        ...solidBorder
+    },
+    button: {
+        paddingHorizontal: 10,
+        alignSelf: 'baseline',
+        ...solidBorder,
+    },
+    menu: {
+        width: 20,
+        height: 20,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...solidBorder
+    },
+    menuItem: {
+        width: 10,
+        height: 1,
+        backgroundColor: '#000',
+        marginVertical: 1
+    }
 });
+
+export default App;
